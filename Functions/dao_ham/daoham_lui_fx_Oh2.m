4cd31fa98b235ea5aab6bcfc03e1af5ff593caa8
TRONGPHUC2003 <trong552003@gmail.com>
2023-12-13 14:35:14 +0700

    Delete Functions directory

--- Functions/dao_ham/daoham_lui_fx_Oh2.m
+++
@@ -1,4 +0,0 @@
-function [dx] = daoham_lui_fx_Oh2(fx, h, x)
-    fx = str2func(['@(x)', fx]);
-    dx = (3 * fx(x) - 4 * fx(x - h) + fx(x - 2 * h)) / (2 * h);
-end
--- Functions/dao_ham/daoham_trungtam_xy_Oh2.m
+++
@@ -1,7 +0,0 @@
-function [dx] = daoham_trungtam_xy_Oh2(xi, yi, x)
-    xi = eval(xi);
-    yi = eval(yi);
-    h = xi(2) - xi(1);
-    x_index = uint16((x - xi(1)) / h + 1); % x_index la vi tri ben trong xi, x = x1 + (x_index - 1) * h
-    dx = (yi(x_index + 1) - yi(x_index - 1)) / (2 * h); % dao ham cua fx
-end
